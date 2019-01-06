da633104c6215af84135eb584b666e14c80a238e
zibraque <zibraque@gmail.com>
2019-01-06 22:15:29 +0800

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="http://www.zibraque.com" target="_blank">Zibraque</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span> </p>
+        <p class="pull-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
